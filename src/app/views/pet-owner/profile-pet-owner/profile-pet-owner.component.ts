08fd4f1f9fa4bac64460ea25fb60208170a441a4
Josehp Herrera <u20211e098@upc.edu.pe>
2024-11-14 20:26:22 -0500

    feat: profile component was added

---
+++ src/app/views/pet-owner/profile-pet-owner/profile-pet-owner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-pet-owner',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-pet-owner.component.html',
+  styleUrl: './profile-pet-owner.component.css'
+})
+export class ProfilePetOwnerComponent {
+
+}
